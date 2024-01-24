ed9876dff7b05583c1ee79531407e5138d805936
vyu <vyuvyuvyu@naver.com>
2024-01-24 23:01:18 +0900

    fix: default select item

--- src/CategoryBoards/ToDoList.tsx
+++ src/CategoryBoards/ToDoList.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { categoriesState, selectedCategoryState, toDoSelector } from '../atoms';
 import { CreateCategory } from './CreateCategory';
 
 function ToDoList() {
   // const toDos = useRecoilValue(toDoSelector);
-  const [selectedCategory, SetSelectedCategory] = useRecoilState(
+  const [selectedCategory, setSelectedCategory] = useRecoilState(
     selectedCategoryState
   );
   const categories = useRecoilValue(categoriesState);
   const handleInput = (event: React.FormEvent<HTMLSelectElement>) => {
-    SetSelectedCategory(event.currentTarget.value as any);
+    setSelectedCategory(event.currentTarget.value as any);
   };
+
+  // console.log('selectedCategory', selectedCategory);
+  useEffect(() => {
+    if (Object.keys(categories).length) {
+      setSelectedCategory(Object.keys(categories)[0]);
+    }
+  }, []);
 
   return (
     <div>
