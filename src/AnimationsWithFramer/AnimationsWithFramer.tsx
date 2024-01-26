25c50fcbf2e65a6849b354ce226cac163e2a85bb
vyu <vyuvyuvyu@naver.com>
2024-01-27 02:03:53 +0900

    refactor: change AnimationsWithFramerPage's backgroundColor

--- src/AnimationsWithFramer/AnimationsWithFramer.tsx
+++ src/AnimationsWithFramer/AnimationsWithFramer.tsx
@@ -73,7 +73,7 @@ const Overlay = styled(motion.div)`
 
 const overlayVariants: Variants = {
   initial: { backgroundColor: 'rgba(0, 0, 0, 0)' },
-  animate: { backgroundColor: 'rgba(55, 158, 143, 0.9)' },
+  animate: { backgroundColor: 'rgba(163, 78, 161, 0.95)' },
   exit: { backgroundColor: 'rgba(0, 0, 0, 0)' },
 };
 
